87c01ab33094dc3b565fda8317114c853614d5f1
ml <30815731+slimlime@users.noreply.github.com>
2020-12-13 10:09:44 +1000

    chore(main): test print line

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/cellois/go-start-disco/cmd"
+import (
+	"github.com/cellois/go-start-disco/cmd"
+)
+
+var (
+	// VERSION set during build?
+	VERSION = "0.0.1"
+)
 
 func main() {
 	cmd.Execute()
+	println("Test2")
+
 }
